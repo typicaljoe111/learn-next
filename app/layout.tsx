4bf049d3d8b6fb2146aa1d4fd9932fd41d472d1f
Keval <typicaljoe111@users.noreply.github.com>
2026-06-15 10:29:26 +0530

    Added new fonts, CSS and hero images

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import "@/app/ui/global.css";
+import { mulish } from "@/app/ui/fonts";
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${mulish.className}`}>{children}</body>
     </html>
   );
 }
